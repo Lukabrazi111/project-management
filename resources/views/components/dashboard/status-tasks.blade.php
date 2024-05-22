5402661bb26c0bb30a80abfa1fbf6070b06d246d
Lukabrazi111 <lukabrazi@redberry.ge>
2024-05-22 19:15:07 +0400

    refactor: dashboard blade files

---
+++ resources/views/components/dashboard/status-tasks.blade.php
@@ -0,0 +1,14 @@
+<div class="grid grid-cols-3 mt-10 gap-2">
+    <div class="bg-gray-800 flex flex-col justify-start space-y-2 px-4 py-3 rounded-lg">
+        <h1 class="font-medium text-yellow-500 text-xl">Pending Tasks</h1>
+        <p class="text-white font-medium">145 / 293</p>
+    </div>
+    <div class="bg-gray-800 flex flex-col justify-start space-y-2 px-4 py-3 rounded-lg">
+        <h1 class="font-medium text-blue-500 text-xl">In Progress Tasks</h1>
+        <p class="text-white font-medium">164 / 311</p>
+    </div>
+    <div class="bg-gray-800 flex flex-col justify-start space-y-2 px-4 py-3 rounded-lg">
+        <h1 class="font-medium text-green-500 text-xl">Completed Tasks</h1>
+        <p class="text-white font-medium">146 / 296</p>
+    </div>
+</div>
